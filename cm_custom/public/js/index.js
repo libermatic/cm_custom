31707be4e69b6c39aaa3f7d7efafe6acdd4c00d8
Sun Howwrongbum <sun@libermatic.com>
2020-12-07 10:47:43 +0000

    0.2.3

--- cm_custom/public/js/index.js
+++ cm_custom/public/js/index.js
@@ -10,7 +10,7 @@ function get_doctype(import_name) {
     .join(' ');
 }
 
-const __version__ = '0.2.2';
+const __version__ = '0.2.3';
 
 frappe.provide('cm_custom');
 cm_custom = { __version__, reports, extensions };
