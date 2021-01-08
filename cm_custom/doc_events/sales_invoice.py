39b2aa0dbfe9a3a6425f2d02d829544bcc938ab9
Sun Howwrongbum <sun@libermatic.com>
2021-01-08 16:22:58 +0000

    fix(Sales Invoice): return outstanding

--- cm_custom/doc_events/sales_invoice.py
+++ cm_custom/doc_events/sales_invoice.py
@@ -6,3 +6,11 @@ import frappe
 def set_missing_values(doc, method, for_validate=False):
     doc.branch = frappe.get_cached_value("POS Profile", doc.pos_profile, "branch")
 
+
+
+
+def before_save(doc, method):
+    if not doc.is_pos:
+        doc.payments = []
+        if doc.is_return:
+            doc.outstanding_amount = 0
