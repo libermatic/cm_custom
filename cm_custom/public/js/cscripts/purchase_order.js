0f99a4697b917b18b81707bd9ca516cc62409f2d
Sun Howwrongbum <sun@libermatic.com>
2021-02-02 11:30:32 +0000

    fix: barcode keycode entry

--- cm_custom/public/js/cscripts/purchase_order.js
+++ cm_custom/public/js/cscripts/purchase_order.js
@@ -1,15 +1,16 @@
 export function purchase_order() {
   return {
     onload: function (frm) {
-      frm.fields_dict.scan_barcode.$input.on('keydown', function (e) {
-        if (e.keyCode === 9 || e.keyCode === 13) {
-          frm
-            .set_value('scan_barcode', e.target.value)
-            .then(() => frm.trigger('scan_barcode'));
-          e.preventDefault();
-          return false;
-        }
-      });
+      frm.fields_dict.scan_barcode.$input &&
+        frm.fields_dict.scan_barcode.$input.on('keydown', function (e) {
+          if (e.keyCode === 9 || e.keyCode === 13) {
+            frm
+              .set_value('scan_barcode', e.target.value)
+              .then(() => frm.trigger('scan_barcode'));
+            e.preventDefault();
+            return false;
+          }
+        });
     },
   };
 }
