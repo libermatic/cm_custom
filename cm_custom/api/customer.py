60e03ddc330c5b516b50f761db2b6fc0bc81e304
Sun Howwrongbum <sun@libermatic.com>
2021-09-14 16:52:10 +0000

    feat: set location while creating Address

--- cm_custom/api/customer.py
+++ cm_custom/api/customer.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from os import name
 import frappe
 from toolz.curried import compose, merge, keyfilter
 from firebase_admin import auth
@@ -6,7 +7,17 @@ from firebase_admin import auth
 from cm_custom.api.firebase import get_decoded_token, app
 from cm_custom.api.utils import handle_error, transform_route
 
-CUSTOMER_FIELDS = ["name", "customer_name", "mobile_no", "email_id"]
+CUSTOMER_FIELDS = ["customer_name", "mobile_no", "email_id"]
+ADDRESS_FIELDS = [
+    "address_line1",
+    "address_line2",
+    "city",
+    "county",
+    "state",
+    "country",
+    "pincode",
+    "location",
+]
 
 
 def get_customer_id(token):
@@ -29,7 +40,7 @@ def get(token):
     doc = frappe.get_doc("Customer", customer_id)
     orders = frappe.db.exists("Sales Order", {"customer": customer_id})
     return merge(
-        keyfilter(lambda x: x in CUSTOMER_FIELDS, doc.as_dict()),
+        keyfilter(lambda x: x in ["name"] + CUSTOMER_FIELDS, doc.as_dict()),
         {"can_register_messaging": bool(orders)},
     )
 
@@ -50,36 +61,19 @@ def create(token, **kwargs):
     if customer_id:
         frappe.throw(frappe._("Customer already created"))
 
-    args = keyfilter(
-        lambda x: x
-        in [
-            "customer_name",
-            "mobile_no",
-            "email_id",
-            "address_line1",
-            "address_line2",
-            "city",
-            "state",
-            "country",
-            "pincode",
-        ],
-        kwargs,
-    )
+    customer_args = keyfilter(lambda x: x in CUSTOMER_FIELDS, kwargs)
+    address_args = keyfilter(lambda x: x in ADDRESS_FIELDS, kwargs)
 
     def insert_or_update():
-        existing = frappe.db.exists("Customer", {"cm_mobile_no": args.get("mobile_no")})
+        existing = frappe.db.exists("Customer", {"cm_mobile_no": customer_args.get("mobile_no")})
         if existing:
             doc = frappe.get_doc("Customer", existing)
             doc.update(
-                {"customer_name": args.get("customer_name"), "cm_firebase_uid": uid}
+                {"customer_name": customer_args.get("customer_name"), "cm_firebase_uid": uid}
             )
-            if args.get("address_line1") and args.get("city"):
-                address = _create_address(existing, args)
-                doc.update({"customer_primary_address": address.get("name")})
-            print([args.get("email_id"), doc.customer_primary_contact])
-            if args.get("email_id") and doc.customer_primary_contact:
+            if customer_args.get("email_id") and doc.customer_primary_contact:
                 contact = frappe.get_doc("Contact", doc.customer_primary_contact)
-                contact.add_email(args.get("email_id"), autosave=True)
+                contact.add_email(customer_args.get("email_id"), autosave=True)
             doc.save(ignore_permissions=True)
             return doc
 
@@ -89,7 +83,7 @@ def create(token, **kwargs):
                 {
                     "doctype": "Customer",
                     "cm_firebase_uid": uid,
-                    "cm_mobile_no": args.get("mobile_no"),
+                    "cm_mobile_no": customer_args.get("mobile_no"),
                     "customer_type": "Individual",
                     "customer_group": frappe.db.get_single_value(
                         "Selling Settings", "customer_group"
@@ -98,15 +92,20 @@ def create(token, **kwargs):
                         "Selling Settings", "territory"
                     ),
                 },
-                args,
+                customer_args,
             )
         ).insert(ignore_permissions=True)
         frappe.set_user(session_user)
         return doc
 
     doc = insert_or_update()
+    if address_args.get("address_line1") and address_args.get("city"):
+        address = _create_address(doc.name, address_args)
+        doc.update({"customer_primary_address": address.get("name")})
+        doc.save(ignore_permissions=True)
+
     auth.set_custom_user_claims(uid, {"customer": True}, app=app)
-    return keyfilter(lambda x: x in CUSTOMER_FIELDS, doc.as_dict())
+    return keyfilter(lambda x: x in ["name"] + CUSTOMER_FIELDS, doc.as_dict())
 
 
 @frappe.whitelist(allow_guest=True)
@@ -118,7 +117,7 @@ def update(token, **kwargs):
     doc = frappe.get_doc("Customer", customer_id)
     doc.update(args)
     doc.save(ignore_permissions=True)
-    return keyfilter(lambda x: x in CUSTOMER_FIELDS, doc.as_dict())
+    return keyfilter(lambda x: x in ["name"] + CUSTOMER_FIELDS, doc.as_dict())
 
 
 @frappe.whitelist(allow_guest=True)
@@ -148,7 +147,8 @@ def list_addresses(token, page="1", page_length="10"):
                 a.county AS county,
                 a.state AS state,
                 a.country AS country,
-                a.pincode AS pincode
+                a.pincode AS pincode,
+                a.location AS location
             FROM `tabDynamic Link` AS dl
             LEFT JOIN `tabAddress` AS a ON a.name = dl.parent
             WHERE dl.parenttype = 'Address' AND
@@ -187,15 +187,7 @@ def get_address(token, name):
     return frappe.get_cached_value(
         "Address",
         name,
-        fieldname=[
-            "name",
-            "address_line1",
-            "address_line1",
-            "city",
-            "state",
-            "country",
-            "pincode",
-        ],
+        fieldname=["name"] + ADDRESS_FIELDS,
         as_dict=1,
     )
 
@@ -208,14 +200,13 @@ def create_address(token, **kwargs):
 
 
 def _create_address(customer, args):
-    fields = ["address_line1", "address_line2", "city", "county", "state", "country", "pincode"]
-    _args = keyfilter(lambda x: x in fields, args)
+    _args = keyfilter(lambda x: x in ADDRESS_FIELDS, args)
     doc = frappe.get_doc(
         merge({"doctype": "Address", "address_type": "Billing"}, _args)
     )
     doc.append("links", {"link_doctype": "Customer", "link_name": customer})
     doc.insert(ignore_permissions=True)
-    return keyfilter(lambda x: x in ["name"] + fields, doc.as_dict())
+    return keyfilter(lambda x: x in ["name"] + ADDRESS_FIELDS, doc.as_dict())
 
 
 @frappe.whitelist(allow_guest=True)
@@ -227,7 +218,7 @@ def delete_address(token, name):
         {"parent": name, "link_doctype": "Customer", "link_name": customer_id},
     ):
         frappe.throw(frappe._("Address not found"))
-        
+
     try:
         frappe.delete_doc("Address", name, ignore_permissions=True)
     except frappe.exceptions.LinkExistsError:
